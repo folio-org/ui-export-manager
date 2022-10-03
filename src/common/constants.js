ce5107e580b5c10a789fff17935b9e79d657efb3
Yury Saukou <yury_saukou@epam.com>
2022-10-03 15:03:49 +0400

    UIEXPMGR-60 Third pane view - Download and re-send actions (#97)
    * UIEXPMGR-60 disable rerun button when integration was deleted
    
    * UIEXPMGR-60 Third pane view - Download and re-send actions
    
    * resolve comments
    
    * resolve comments and lint errors

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
-const EXPORT_JOB_STATUSES = ['SCHEDULED', 'IN_PROGRESS', 'SUCCESSFUL', 'FAILED'];
+export const EXPORT_JOB_STATUSES = {
+  scheduled: 'SCHEDULED',
+  inProgress: 'IN_PROGRESS',
+  successful: 'SUCCESSFUL',
+  failed: 'FAILED',
+};
 
-export const EXPORT_JOB_STATUS_OPTIONS = EXPORT_JOB_STATUSES.map(status => ({
+export const EXPORT_JOB_STATUS_OPTIONS = Object.values(EXPORT_JOB_STATUSES).map(status => ({
   value: status,
   label: <FormattedMessage id={`ui-export-manager.exportJob.status.${status}`} />,
 }));
+
+export const EXPORT_CONFIGS_API = 'data-export-spring/configs';
+export const EXPORT_JOBS_API = 'data-export-spring/jobs';
---
+++ src/common/hooks/useExportConfig/useExportConfig.js
@@ -0,0 +1,31 @@
+import { useQuery } from 'react-query';
+
+import {
+  useNamespace,
+  useOkapiKy,
+} from '@folio/stripes/core';
+
+import { EXPORT_CONFIGS_API } from '../../constants';
+
+export const useExportConfig = (id) => {
+  const ky = useOkapiKy();
+  const [namespace] = useNamespace({ key: 'job-configs' });
+
+  const {
+    data = {},
+    isError,
+    isLoading,
+  } = useQuery(
+    [namespace, id],
+    () => ky.get(`${EXPORT_CONFIGS_API}/${id}`).json(),
+    {
+      enabled: Boolean(id),
+    },
+  );
+
+  return {
+    config: data,
+    isError,
+    isLoading,
+  };
+};
