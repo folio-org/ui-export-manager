8c89dc17b95c9af43df805ac239f5b07552fd951
vashjs <vadym.shchekotilin@gmail.com>
2024-08-30 14:24:41 +0200

    UIEXPMGR-110 Rollback keep data

--- src/ExportEdiJobs/apiQuery.js
+++ src/ExportEdiJobs/apiQuery.js
@@ -49,7 +49,6 @@ export const useExportEdiJobsQuery = (search, pagination, filters) => {
 
       return { ...response };
     },
-    keepPreviousData: true,
     enabled: !![...Object.values(filters)].filter(Boolean).length,
     getNextPageParam: (lastPage) => lastPage.nextPage,
   });
