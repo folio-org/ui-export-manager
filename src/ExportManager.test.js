4e964d329f40a42e430ddca6e88121da05ecc291
Yury Saukou <yury_saukou@epam.com>
2025-06-09 18:32:06 +0400

    update tests

--- src/ExportManager.test.js
+++ src/ExportManager.test.js
@@ -1,17 +1,16 @@
+import '@folio/stripes-acq-components/test/jest/__mock__';
+
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 
 import { runAxeTest } from '@folio/stripes-testing';
-import '@folio/stripes-acq-components/test/jest/__mock__';
+import { useStripes } from '@folio/stripes/core';
 
 import { ExportManager } from './ExportManager';
 
-jest.mock('./ExportJobs', () => {
-  return {
-    ExportJobs: () => <span>ExportJobs</span>,
-  };
-});
+jest.mock('./ExportJobs', () => ({ ExportJobs: () => <span>ExportJobs</span> }));
+jest.mock('./ExportEdiJobs', () => ({ ExportEdiJobs: () => <span>ExportEdiJobs</span> }));
 
 const renderExportManager = (path) => (render(
   <MemoryRouter
@@ -22,7 +21,19 @@ const renderExportManager = (path) => (render(
   </MemoryRouter>,
 ));
 
+const stripesMock = {
+  hasInterface: jest.fn(() => true),
+};
+
 describe('ExportManager', () => {
+  beforeEach(() => {
+    useStripes.mockReturnValue(stripesMock);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should be display ExportJobs when path is /export-manager/jobs', () => {
     const { getByText } = renderExportManager('/export-manager/jobs');
 
@@ -63,5 +74,19 @@ describe('ExportManager', () => {
     await runAxeTest({
       rootNode: container,
     });
+  });
+
+  it('should render Organizations EDI Jobs when path is /export-manager/edi-jobs', () => {
+    const { getByText } = renderExportManager('/export-manager/edi-jobs');
+
+    expect(getByText('ExportEdiJobs')).toBeInTheDocument();
+  });
+
+  it('should redirect to ExportJobs when path is /export-manager/edi-jobs and stripes does not have organizations interface', () => {
+    stripesMock.hasInterface.mockReturnValue(false);
+
+    const { getByText } = renderExportManager('/export-manager/edi-jobs');
+
+    expect(getByText('ExportJobs')).toBeInTheDocument();
   });
 });
