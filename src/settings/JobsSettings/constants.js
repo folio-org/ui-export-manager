ccd4576d0744ba3b852c125227f74f9762fb96da
Yury Saukou <yury_saukou@epam.com>
2025-08-07 18:32:22 +0400

    UIEXPMGR-129 Update job type labels

--- src/settings/JobsSettings/constants.js
+++ src/settings/JobsSettings/constants.js
@@ -5,17 +5,18 @@ const {
   BURSAR_FEES_FINES,
   CIRCULATION_LOG,
   E_HOLDINGS,
-  ORDERS_CSV,
-  ORDERS_EDI,
 } = EXPORT_JOB_TYPE_KEYS;
+
+const EDIFACT = 'EDIFACT_ORDERS_EXPORT';
+const CLAIMS = 'CLAIMS';
 
 export const TRANSLATION_KEYS_DICT = {
   [AUTH_HEADINGS_UPDATES]: `ui-export-manager.exportJob.type.${AUTH_HEADINGS_UPDATES}`,
   [BURSAR_FEES_FINES]: `ui-export-manager.exportJob.type.${BURSAR_FEES_FINES}`,
   [CIRCULATION_LOG]: `ui-export-manager.exportJob.type.${CIRCULATION_LOG}`,
   [E_HOLDINGS]: `ui-export-manager.exportJob.type.${E_HOLDINGS}`,
-  EDIFACT_ORDERS_EXPORT: `ui-export-manager.exportJob.type.${ORDERS_EDI}`,
-  CLAIMS: `ui-export-manager.exportJob.type.${ORDERS_CSV}`,
+  [EDIFACT]: `ui-export-manager.exportJob.details.type.${EDIFACT}`,
+  [CLAIMS]: 'ui-export-manager.exportJob.integrationType.claiming',
 };
 
 export const DELETION_INTERVALS_FIELD_ARRAY_NAME = 'jobDeletionIntervals';
